1d77805101edb56e686738a00a2b24655060bdf9
Leli <lelilia@posteo.de>
2020-12-06 11:43:52 +0100

    day6 cleaned

--- day6.py
+++ day6.py
@@ -1,26 +1,23 @@
 ''' ADVENT OF CODE day 6'''
 
+from functools import reduce
+
 def get_count_any(block):
     '''count the number of unique answers in a block'''
     return len(set(block.replace('\n', '')))
 
 def get_count_all(block):
     '''count the number of answers that everyone gave in a block'''
-    block = block.split('\n')
-    set_everyone =  set(block.pop())
-    while len(block) > 0:
-        this_answer = set(block.pop())
-        set_everyone = set_everyone.intersection(this_answer)
-    return len(set_everyone)
+    return len(reduce(lambda x,y: set(x).intersection(set(y)), block.split("\n")))
 
 
 if __name__=='__main__':
 
     with open('input/day6.txt') as f:
-        answers = f.read().split('\n\n')
+        blocks = f.read().split('\n\n')
 
-    sum_any = sum(map(get_count_any, answers))
-    sum_all = sum(map(get_count_all, answers))
+    sum_any = sum(map(get_count_any, blocks))
+    sum_all = sum(map(get_count_all, blocks))
 
     print(f'Part 1: {sum_any}')
     print(f'Part 2: {sum_all}')
